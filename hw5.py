2c0d36c0545c6a9e1fd0adb871d841f84ae91074
Shelsiyou <144706105+Shelsiyou@users.noreply.github.com>
2023-10-21 22:15:37 +0300

    try again

---
+++ hw5.py
@@ -0,0 +1,79 @@
+
+class sequence(object):
+    def __init__(self, string3, string4: str):
+        self.string1= sqe1
+        self.string2= sqe2
+    def count_nucleotides (self):
+        a=0
+        t=0
+        g=0
+        c=0
+        count=0
+        while count<= len(self.string1):
+            for i in self.string1:
+                if i=='A' or i=='a':
+                    a+=1
+                if i=='G' or i=='g':
+                    g+=1
+                if i=='T' or i=='t':
+                    t+=1
+                    count+=1
+                if i=='C' or i=='c':
+                    c+=1
+                count+=1
+        d={'A:': a, 'G:': g, 'C:': c, 'T:': t}
+        return d
+            
+    def transcribe(self):
+        return NameError('бред')
+    
+    def complement_dna(self) -> str:
+        compl=''
+        for i in self.string1: 
+            if i=='A' or i=='a':
+                compl+= 't'
+            if i=='G' or i=='g':
+                compl+= 'c'
+            if i=='T' or i=='t':
+                compl+= 'a'
+            if i=='C' or i=='c':
+                compl+= 'G'
+        complrev = compl[::-1]
+        return complrev
+    def hamming_distance(self):
+        def counter(m, n):
+            if m != n:
+                return 1
+            return 0
+        s = list(map(counter, self.string1, self.string2))
+        return sum(s)
+        
+class DNA(sequence):
+    def transcribe(self):
+        tseq=''
+        count=1
+        while count<=len(self.string1):
+            for i in self.string1:
+                if i=='t':
+                    tseq += 'u'
+                else: tseq+= i
+                count+=1       
+        return tseq
+
+class RNA(sequence):
+    def transcribe(self):
+        tseq=''
+        count=1
+        while count<=len(self.string1):
+            for i in self.string1:
+                if i=='u':
+                    tseq += 't'
+                else: tseq+= i
+                count+=1       
+        return tseq
+        
+
+sqe1=str(input()) 
+sqe2=str(input())
+sqe= DNA(sqe1, sqe2)
+print(sqe.count_nucleotides(), sqe.complement_dna, sqe.hamming_distance, sqe.transcribe)
